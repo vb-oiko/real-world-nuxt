53b16630ef49526fadec90c73833211b621b79ef
Vasyl Boyko <vboiko0@gmail.com>
2020-04-15 17:55:10 +0300

    add: nested route

---
+++ pages/event/create.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Where you can create a new event taking place in yuor neighborhood'
+        }
+      ]
+    }
+  }
+})
+</script>
