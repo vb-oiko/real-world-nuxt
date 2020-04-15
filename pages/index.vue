b424862511838f472a8a560889b90283bdb5be8d
Vasyl Boyko <vboiko0@gmail.com>
2020-04-15 17:14:13 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Events</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  }
+})
+</script>
