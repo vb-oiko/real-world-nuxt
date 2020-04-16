f184a4c333e84cc66154f6f335237fbe752233b6
Vasyl Boyko <vboiko0@gmail.com>
2020-04-16 12:43:43 +0300

    add: single vent page API call

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,27 +1,50 @@
 <template>
   <div>
-    <h1>Event #{{ id }}</h1>
+    <h1>{{ event.title }}</h1>
+    <event-card :event="event" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import EventCard from '@/components/EventCard.vue'
 
 export default Vue.extend({
-  computed: {
-    id(): string {
-      return this.$route.params.id
+  components: {
+    EventCard
+  },
+
+  async asyncData(context: any) {
+    const response = await context.$axios
+      .get('http://localhost:3001/events/' + context.params.id)
+      .catch(() => {
+        context.error({
+          statusCode: 503,
+          message: 'Unable to fetch event #' + context.params.id
+        })
+      })
+
+    if (response && response.data) {
+      return {
+        event: response.data
+      }
     }
+
+    context.error({
+      statusCode: 404,
+      message: 'Unable to find event #' + context.params.id
+    })
   },
 
   head() {
     return {
-      title: 'Event #' + (this as any).id,
+      title: (this as any).event.title,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: 'What you need to know about event #' + (this as any).id
+          content:
+            'What you need to know about event: ' + (this as any).event.title
         }
       ]
     }
--- pages/index.vue
+++ pages/index.vue
@@ -13,20 +13,20 @@ export default Vue.extend({
   components: {
     EventCard
   },
-  asyncData(context) {
-    return (context as any).$axios
+
+  async asyncData(context: any) {
+    const { data } = await context.$axios
       .get('http://localhost:3001/events')
-      .then((response: any) => {
-        return {
-          events: response.data
-        }
-      })
       .catch(() => {
         context.error({
           statusCode: 503,
           message: 'Unable to fetch events this time. Please try again.'
         })
       })
+
+    return {
+      events: data
+    }
   },
 
   head() {
